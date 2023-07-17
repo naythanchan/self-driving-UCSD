43e25cea023ad0942a400b826ab826d01f701419
Naythan Chan <48609695+naythan-c@users.noreply.github.com>
2023-07-17 15:40:32 -0700

    we got ittt

---
+++ cloud_driver.py
@@ -0,0 +1,139 @@
+import pybullet as p
+import time
+import pybullet_data
+import math
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+# Physics
+physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
+p.resetSimulation()
+p.setGravity(0, 0, -9.8)
+p.setRealTimeSimulation(1)  # either this
+
+# Camera
+p.resetDebugVisualizerCamera(
+    cameraDistance=0.1,
+    cameraYaw=- 90,
+    cameraPitch=10,
+    cameraTargetPosition=[
+        0, 0, 0.2],
+    physicsClientId=0
+)
+# Path
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# Assets
+p.loadSDF("stadium.sdf")
+car = p.loadURDF("racecar/racecar.urdf")
+cube = p.loadURDF("cube.urdf", basePosition=[0, 3, 0.5], useFixedBase=1)
+
+# Obstacles
+zone_width = 6  # meters
+zone_depth = 10  # meters
+
+# Define the race position
+race_position = [0, 0, 0]  # [x, y, z]
+
+# Generate a random position within the zone
+def generate_random_position():
+    x = random.uniform(race_position[0] + 2, race_position[0] + 2 + zone_depth)
+    y = random.uniform(
+        race_position[1] - zone_width/2, race_position[1] + zone_width/2)
+    z = race_position[2] + 0.5  # Height of the cube
+    return [x, y, z]
+
+num_cubes = 10  # Number of cubes to load
+box_shape = p.createCollisionShape(p.GEOM_CYLINDER, radius=0.4, height=1)
+
+for _ in range(num_cubes):
+    cube_position = generate_random_position()
+    box_body = p.createMultiBody(
+        baseMass=1.0,
+        baseCollisionShapeIndex=box_shape,
+        basePosition=cube_position,
+    )
+
+# While Loop Init
+start_time = time.time()
+last_process_time = 0
+i = 0
+
+steering = [4, 6]
+wheels = [2, 3]
+target_pos = 0
+
+# Loop
+while (True):
+    elapsed_time = time.time() - start_time
+
+    # Depth Camera
+    p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
+    cam_info = p.getDebugVisualizerCamera()
+    view_matrix = cam_info[2]
+    proj_matrix = cam_info[3]
+
+    width, height, rgb_img, depth_img, seg_img = p.getCameraImage(
+        256, 256, viewMatrix=view_matrix, projectionMatrix=proj_matrix
+    )
+    depth = np.array(depth_img).reshape((256, 256))
+
+    # Driving
+    for wheel in wheels:
+        p.setJointMotorControl2(car,
+                                wheel,
+                                p.VELOCITY_CONTROL,
+                                targetVelocity=25,
+                                force=2)
+    for steer in steering:
+        p.setJointMotorControl2(
+            car, steer, p.POSITION_CONTROL, targetPosition=0)
+
+    
+    # Localization
+    pos, hquat = p.getBasePositionAndOrientation(car)
+    h = p.getEulerFromQuaternion(hquat)
+    x = pos[0]
+    y = pos[1]
+
+    if elapsed_time - last_process_time >= 2.0:
+        # Point Cloud
+        projection = np.array(proj_matrix).reshape(4, 4)
+        fx = projection[0][0]
+        fy = -projection[1][1]
+        cx = projection[0][2]
+        cy = projection[1][2]
+
+        # Generate point cloud
+        v, u = np.indices(depth.shape)
+        z = depth.copy()
+        valid_mask = (z > 0)
+        x = (u - cx) * z / fx
+        y = (v - cy) * z / fy + 150
+        point_cloud = np.column_stack((x[valid_mask], y[valid_mask], 10000 - z[valid_mask] * 10000))
+        obstacles = point_cloud[(point_cloud[:, 2] != 0) & (point_cloud[:, 1] > 0)]
+
+        # Remove outliers
+        
+        # Make it 2d
+        plt.scatter(obstacles[:, 0], obstacles[:, 1], s=1)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Obstacles')
+        plt.savefig(f'pcd_images/pcd_{i}.png')
+
+        # Update
+        i += 1
+        last_process_time = elapsed_time
+
+    # Follow Robot With Camera
+    p.resetDebugVisualizerCamera(
+        cameraDistance=0.1,
+        cameraYaw=math.degrees(h[2]) - 90,
+        cameraPitch=10,
+        cameraTargetPosition=[
+            pos[0] + math.cos(h[2]), pos[1] + math.sin(h[2]), pos[2] + 0.2],
+        physicsClientId=0
+    )
--- pcd.py
+++ pcd.py
@@ -6,16 +6,11 @@ import open3d as o3d
 with open('depth_output.json', 'r') as f:
     data = json.load(f)
 
-# Access specific values from the JSON data
+# Retrieve depth matrices
 depth_img = data['depth_img']
 view_matrix = data['view_matrix']
 proj_matrix = np.array(data['proj_matrix']).reshape(4, 4)
 depth = np.array(depth_img)
-
-# Check the shape of the depth array
-# print(depth.shape)
-# print(proj_matrix)
-
 
 # Camera intrinsics
 fx = proj_matrix[0][0]
@@ -24,31 +19,34 @@ cx = proj_matrix[0][2]
 cy = proj_matrix[1][2]
 
 # Generate point cloud
-point_cloud = []
-for v in range(depth.shape[0]):
-    for u in range(depth.shape[1]):
-        z = depth[v, u]
-        if z > 0:
-            x = (u - cx) * z / fx
-            y = (v - cy) * z / fy
-            point_cloud.append([x, y, 10000 - z * 10000])
+v, u = np.indices(depth.shape)
+z = depth.copy()
+valid_mask = (z > 0)
 
-# Convert to NumPy array
-point_cloud = np.array(point_cloud)
+x = (u - cx) * z / fx
+y = (v - cy) * z / fy + 150
+point_cloud = np.column_stack((x[valid_mask], y[valid_mask], 10000 - z[valid_mask] * 10000))
+obstacles = point_cloud[(point_cloud[:, 2] != 0) & (point_cloud[:, 1] > 0)]
 
-filtered_point_cloud = point_cloud[point_cloud[:, 2] != 0]
-print(filtered_point_cloud)
-filtered_point_cloud_ground = filtered_point_cloud[(filtered_point_cloud[:, 1] > -150)]
-
+# Save output
 np.set_printoptions(threshold=np.inf)
 data = {
-    'point_cloud': filtered_point_cloud.tolist()
+    'obstacles_point_cloud': obstacles.tolist()
 }
 with open('point_cloud.json', 'w') as f:
     json.dump(data, f)
 
-pcd_o3d = o3d.geometry.PointCloud()  # create point cloud object
-pcd_o3d.points = o3d.utility.Vector3dVector(
-    filtered_point_cloud_ground)  # set pcd_np as the point cloud points
-# Visualize:
+# Axis array
+x_axis = np.array([[0, 0, 0], [200, 0, 0], [250, 0, 0], [300, 0, 0]])
+y_axis = np.array([[0, 0, 0], [0, 50, 0], [0, 100, 0], [0, 150, 0], [0, 200, 0], [0, 250, 0], [0, 300, 0]])
+z_axis = np.array([[0, 0, 0], [0, 0, 200], [0, 0, 250], [0, 0, 300]])
+
+# 2D map
+swapped_coordinates = obstacles.copy()  # Create a copy to avoid modifying the original array
+swapped_coordinates[:, 1], swapped_coordinates[:, 2] = obstacles[:, 2], obstacles[:, 1]
+swapped_coordinates[:, 2] = 0
+
+# Generate visualization
+pcd_o3d = o3d.geometry.PointCloud()
+pcd_o3d.points = o3d.utility.Vector3dVector(obstacles)
 o3d.visualization.draw_geometries([pcd_o3d])
